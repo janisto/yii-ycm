38beaec4f5969ad88a4b5cd7216efc3475e4ae84
Johnny <wu-xuesong@qq.com>
2013-05-01 19:13:33 +0800

    Add zh_CN

--- messages/config.php
+++ messages/config.php
@@ -10,7 +10,7 @@
 return array(
 	'sourcePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'messagePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'messages',
-	'languages'=>array('fi'),
+	'languages'=>array('fi','zh_cn'),
 	'fileTypes'=>array('php'),
 	'overwrite'=>false,
 	'removeOld'=>true,
